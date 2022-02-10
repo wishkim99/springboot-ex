43130577eca7368cfe90441171522f92256bca1d
wishkim99 <zzsowon@naver.com>
2022-02-10 11:50:03 +0900

    init

---
+++ src/main/java/com/poscoict/springbootex/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.poscoict.springbootex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * 
+ * Bootstrap Class
+ * 
+ * 
+ *
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
